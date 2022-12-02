7e9c1d82d5c6eb169d36cc0a3408b121ed144756
taelliee <elitza_@mail.bg>
2022-12-02 19:29:15 +0200

    fundamentals exercises

---
+++ Regular Expressions - Lab 2 dec 22/01. Match Full Name/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            Regex regex = new Regex(pattern);
+            string text = Console.ReadLine();
+            MatchCollection matches = regex.Matches(text);
+
+            Console.WriteLine(string.Join(" ", matches));
+        }
+    }
+}
---
+++ Regular Expressions - Lab 2 dec 22/03. Match Dates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string pattern = @"\b(?<day>\d{2})(.|/|-])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+            //неименуваните групи застават пред именуваните --> 1 група
+            //нулевата група съдържа целия match
+            
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(text);
+            foreach (Match match in matches)
+            {
+                var day = match.Groups["day"].Value;
+                var month = match.Groups["month"].Value;
+                var year = match.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
