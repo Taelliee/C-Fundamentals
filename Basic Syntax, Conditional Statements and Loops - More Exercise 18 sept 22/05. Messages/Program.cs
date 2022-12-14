36641b4db4dd2eaabcdce3b6fe585a300d43bf50
Elitsa Ilarionova <114700525+Taelliee@users.noreply.github.com>
2022-12-14 18:33:33 +0200

    Update Program.cs

--- Basic Syntax, Conditional Statements and Loops - More Exercise 18 sept 22/05. Messages/Program.cs
+++ Basic Syntax, Conditional Statements and Loops - More Exercise 18 sept 22/05. Messages/Program.cs
@@ -8,33 +8,33 @@ namespace _05._Messages
         static void Main(string[] args)
 
         {
-			//offset = (main digit - 2) * 3
-			//letter index = (offset + digit length - 1)
+		//offset = (main digit - 2) * 3
+		//letter index = (offset + digit length - 1)
 
             int n = int.Parse(Console.ReadLine());
             string letters = string.Empty;
             string input;
 
-			for (int i = 0; i < n; i++)
-			{
+		for (int i = 0; i < n; i++)
+		{
                 input = Console.ReadLine();
                 int mainDigit = int.Parse(input[0].ToString());
 
                 if (mainDigit == 0)
                 {
                     letters += " "; continue;
-				}
+		}
 
-				int offset = (mainDigit - 2) * 3;
+		int offset = (mainDigit - 2) * 3;
 
                 if (mainDigit == 8 || mainDigit == 9)
                 {
                     offset++;
-				}
+		}
 
                 int letterIndex = (offset + input.Length - 1);
                 letters += (char)(97 + letterIndex);
-			}
+		}
 
             Console.WriteLine(letters);
         }
